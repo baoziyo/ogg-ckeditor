f398618192772d32e0c9c97169931e4c4b9af14c
ogg <baoziyoo@gmail.com>
2021-11-22 15:08:15 +0800

    feat:# update

--- plugins/uploadpictures/dialogs/uploadpictures.js
+++ plugins/uploadpictures/dialogs/uploadpictures.js
@@ -1,29 +1,31 @@
-CKEDITOR.dialog.add('uploadpictures', function(editor) {
-    
+CKEDITOR.dialog.add('uploadpictures', function (editor) {
+
     var imageHtml = '', uploader;
     var lang = editor.lang.uploadpictures;
 
     var initEvent = function () {
-      function receiveMessage(event) {
-          var eventName = event.data.eventName;
-          if (eventName === 'es-ckeditor.post') {
-              var innerHtml = event.data.html;
-              $('.' + editor.id + ' #js-uploadpictures-body').append(innerHtml);
-              try{
-                $(document.getElementById("uploadContainer_" + editor.name))[0].remove();
-              }catch(err){
-                $(document.getElementById("uploadContainer_" + editor.name))[0].removeNode(true);
-              };
+        function receiveMessage(event) {
+            var eventName = event.data.eventName;
+            if (eventName === 'es-ckeditor.post') {
+                var innerHtml = event.data.html;
+                $('.' + editor.id + ' #js-uploadpictures-body').append(innerHtml);
+                try {
+                    $(document.getElementById("uploadContainer_" + editor.name))[0].remove();
+                } catch (err) {
+                    $(document.getElementById("uploadContainer_" + editor.name))[0].removeNode(true);
+                }
+                ;
 
-              onLoadDialog();
-          }
-      }
-      window.addEventListener("message", receiveMessage, false);
+                onLoadDialog();
+            }
+        }
+
+        window.addEventListener("message", receiveMessage, false);
     };
 
     initEvent();
 
-    var onLoadDialog = function() {
+    var onLoadDialog = function () {
 
         var uploadUrl = editor.config.filebrowserImageUploadUrl;
         uploadUrl += uploadUrl.indexOf('?') == -1 ? '?' : '&';
@@ -46,7 +48,7 @@ CKEDITOR.dialog.add('uploadpictures', function(editor) {
             resize: false,
             fileNumLimit: 10,
             threads: 1,
-            fileSingleSizeLimit: fileSingleSizeLimit*1024*1024,
+            fileSingleSizeLimit: fileSingleSizeLimit * 1024 * 1024,
             accept: {
                 title: 'Images',
                 extensions: 'gif,jpg,jpeg,bmp,png,ico',
@@ -54,11 +56,11 @@ CKEDITOR.dialog.add('uploadpictures', function(editor) {
             }
         });
 
-        $('.js-start-upload-btn').on('click', function() {
+        $('.js-start-upload-btn').on('click', function () {
             uploader.upload();
         });
 
-        uploader.on('error', function(errorCode) {
+        uploader.on('error', function (errorCode) {
             if (errorCode == 'Q_TYPE_DENIED') {
                 alert(lang.file_type_tip + uploader.options.accept[0].extensions.join(','));
             } else if (errorCode == 'F_EXCEED_SIZE') {
@@ -66,7 +68,7 @@ CKEDITOR.dialog.add('uploadpictures', function(editor) {
             }
         });
 
-        uploader.on('fileQueued', function(file) {
+        uploader.on('fileQueued', function (file) {
             $('.' + editor.id + ' .balloon-nofile').remove();
             var $list = $('.' + editor.id + ' .balloon-filelist ul');
             $list.append(
@@ -80,21 +82,21 @@ CKEDITOR.dialog.add('uploadpictures', function(editor) {
             );
         });
 
-        $('.' + editor.id + ' .balloon-filelist').delegate('.file-remove', 'click', function(){
+        $('.' + editor.id + ' .balloon-filelist').delegate('.file-remove', 'click', function () {
             uploader.removeFile($(this).parent('li').attr('id'), true);
             $(this).parent().remove();
         });
 
-        uploader.on('uploadProgress', function(file, percentage) {
+        uploader.on('uploadProgress', function (file, percentage) {
             var $li = $('.' + editor.id + ' #' + file.id);
             percentage = (percentage * 100).toFixed(2) + '%';
             $li.find('.file-status').html(percentage);
             $li.find('.file-progress-bar').css('width', percentage);
         });
 
-        uploader.on('uploadSuccess', function(file, response) {
+        uploader.on('uploadSuccess', function (file, response) {
             console.log('file', response);
-            imageHtml += '<p><img src="' + response.url  + '" /></p>';
+            imageHtml += '<p><img src="' + response.url + '" data-uri="' + response.uri + '" /></p>';
 
             var $li = $('.' + editor.id + ' #' + file.id);
             $li.find('.file-status').html(lang.uploaded_text);
@@ -103,11 +105,11 @@ CKEDITOR.dialog.add('uploadpictures', function(editor) {
         });
 
         // uploader.on('uploadFinished', function() {
-            
+
         // });
     };
 
-    var url = CKEDITOR.getUrl('plugins/uploadpictures/html/index_'+editor.config.language+'.html');
+    var url = CKEDITOR.getUrl('plugins/uploadpictures/html/index_' + editor.config.language + '.html');
     var dialogHtml = '<div id="js-uploadpictures-body"><iframe src=' + url + ' scrolling="no" id="uploadContainer_' + editor.name + '" width="0" height="0" style="display:none;visibility:hidden"></iframe></div>';
 
     var dialogDefinition = {
@@ -127,12 +129,12 @@ CKEDITOR.dialog.add('uploadpictures', function(editor) {
                 html: dialogHtml
             }]
         }],
-        
-        onLoad: function() {
+
+        onLoad: function () {
             $('.' + editor.id + ' .js-uploadpictures-body').css({'vertical-align': 'top'});
         },
 
-        onOk: function() {
+        onOk: function () {
             if (uploader.isInProgress()) {
                 alert(lang.waiting_finish_tip);
                 return false;
@@ -152,7 +154,7 @@ CKEDITOR.dialog.add('uploadpictures', function(editor) {
             uploader.reset();
             $('.' + editor.id + ' .balloon-filelist ul').empty();
         }
-       
+
     };
 
     return dialogDefinition;
